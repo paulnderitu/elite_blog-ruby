8267f04074b091c66612ab9296c76d7bb6e73cf1
Your Name <youremail@domain.com>
2017-08-03 10:54:35 +0300

    introduced rails relationships between my questions and answers. Then introduced created functionalities of crud for my answers. Next should be patch and delete

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,22 @@
+class AnswersController < ApplicationController
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to question_path(@answer.question)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:description)
+  end
+end
