1ac13c3a42de5ec5c1e78c75521ccf8e2f1a5164
Your Name <youremail@domain.com>
2017-08-03 11:14:12 +0300

    added patch and delete functionalities and mapped them in controllers and views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -14,6 +14,28 @@ class AnswersController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    if @answer.update(answer_params)
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    @answer.destroy
+    redirect_to question_path(@question)
+  end
+
   private
 
   def answer_params
