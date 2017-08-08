7c172895b8428c4fa4bf31cb9f51e7151bc21284
Your Name <youremail@domain.com>
2017-08-08 11:19:46 +0300

    added methods for creating a user account and authenticating that usr in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,34 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   has_many :answers
   has_many :questions
+
+  # users.password_hash in the database is a :string
+  include bcrypt
+
+  def password
+    @password ||= password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = password.create(new_password)
+    self.password_hash = @password
+  end
+
+  # creating an account
+
+  def create
+    @user = User.new(params[:user])
+    @user.password = params[:password]
+    @user.save!
+  end
+
+  # authenticating a user
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      give_token
+    else
+      redirect_to home_url
+    end
+  end
 end
