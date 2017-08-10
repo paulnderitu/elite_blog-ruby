cce5fd59bbd9ef76484b16f613f5a0baf30f9157
Your Name <youremail@domain.com>
2017-08-10 14:11:21 +0300

    added a user login successfully by adding a create method to the sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  # attr_accessible :email, :username, :password, :password_confirmation
+
   validates_uniqueness_of :email
   has_many :answers
   has_many :questions
