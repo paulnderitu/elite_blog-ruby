f42d09c420886f7e41b4ab379e9146712ecd51e4
Your Name <youremail@domain.com>
2017-08-10 12:53:00 +0300

    added fields and methods to accomated the new params of a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :email, :username, :password, :password_confirmation, :question_id)
   end
 end
