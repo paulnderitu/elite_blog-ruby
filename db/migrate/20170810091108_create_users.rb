de693e9fff23344287b63771983bd09b6e0f00f9
Your Name <youremail@domain.com>
2017-08-10 12:20:19 +0300

    dropped the users table and created it afresh to accomodatedpassword_digest to give room for authentication

--- db/migrate/20170810091108_create_users.rb
+++ db/migrate/20170810091108_create_users.rb
@@ -2,6 +2,10 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.column :name, :string
+      t.column :email, :string
+      t.column :username, :string
+      t.column :password_digest, :string
+      t.column :question_id, :integer
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803131708) do
+ActiveRecord::Schema.define(version: 20170810091108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20170803131708) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
+    t.string "username"
+    t.string "password_digest"
+    t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
