f30641d17d271af1405d167143f367f46dd2599b
Your Name <youremail@domain.com>
2017-08-10 13:00:52 +0300

    specified that the user has a secure password. also identified accessible user attributes which are email, username, password and password confirmation. also validated the uniqueness of email. Also added a flash message on user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = 'Thanks for signing up!'
       redirect_to users_path
     else
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  attr_accessible :email, :username, :password, :password_confirmation
+
+  validates_uniqueness_of :email
   has_many :answers
   has_many :questions
 end
