0ce5d517ecdc47bb8a32d00d4f9c3ab7934437f7
Your Name <youremail@domain.com>
2017-08-10 11:55:32 +0300

    Refactored to the last point where my project wasworking before introducing bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,37 +1,4 @@
-require 'bcrypt'
-
 class User < ActiveRecord::Base
   has_many :answers
   has_many :questions
-  has_secure_password
-
-  # users.password_hash in the database is a :string
-  include bcrypt
-
-  def password
-    @password ||= password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = password.create(new_password)
-    self.password_hash = @password
-  end
-
-  # creating an account
-
-  def create
-    @user = User.new(params[:user])
-    @user.password = params[:password]
-    @user.save!
-  end
-
-  # authenticating a user
-  def login
-    @user = User.find_by_email(params[:email])
-    if @user.password == params[:password]
-      give_token
-    else
-      redirect_to home_url
-    end
-  end
 end
