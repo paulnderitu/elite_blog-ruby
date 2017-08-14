d809d6f6f53f0d6c3c68383b5f76eedbbbf3f6c1
Your Name <youremail@domain.com>
2017-08-14 12:25:21 +0300

    updated sessions changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = 'Logged in!'
+      # flash[:notice] = 'Logged in!'
       redirect_to root_url
     else
       flash.now.alert = 'Email or password is invalid'
